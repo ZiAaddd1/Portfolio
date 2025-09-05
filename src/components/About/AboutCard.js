6b094d24ddc49cd8675c0efd665ecc0093aa224e
Ziad Ashraf <ziaddd155@gmail.com>
2025-09-06 02:10:09 +0300

    first commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,40 +1,22 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+<div style={{ textAlign: "justify" }}>
+  Hi Everyone, I am <span className="purple">Ziad Ashraf </span>
+  from <span className="purple"> Cairo, Egypt.</span>
+  <br />
+  <br />
+  I’m a passionate Front-End Developer specializing in React, Angular, JavaScript, HTML, CSS, and Bootstrap. I love creating modern, responsive, and user-friendly web applications with clean code and attractive designs.
+  <br />
+  <br />
+  I’m always eager to learn new technologies, improve my skills, and turn ideas into interactive digital experiences. My goal is to grow as a Front-End Engineer and contribute to impactful projects that make a difference.
+</div> <br />
+<footer className="blockquote-footer">Ziad Ashraf</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/ZiAaddd1"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/ziad-ashraf-977226290/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/__ziad_ashraf_1"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
